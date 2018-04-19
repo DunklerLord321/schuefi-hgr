c37fa9cdfa2fd345e88e1a0f9303013eeadad406
Yajo Web <yajo10@yahoo.de>
2018-04-19 18:22:44 +0200

    Version 2.0.7.2-2
    Änderungen:
    
    --Überarbeitung der Filterung zur Ausgabe der Finanzen: mehrere Flter gleicheitig (z.T. sinnlose) sind möglich, korrektes Löschen von Filtern

--- scripts/inc.output_finanz.php
+++ scripts/inc.output_finanz.php
@@ -4,51 +4,94 @@ if (isset($user) && $user->runscript()) {
 	echo "<p>Filter:<br>";
 	$filter = array();
 	$filter[] = array(
+			'id' => 1,
 			'row' => 'konto_bar',
-			'property' => 'konto',
+			'property' => '\'konto\'',
 			'typ' => 'enum',
+			'compare' => "=",
 			'label' => 'nur Einträge des Kontobuches'
 	);
 	$filter[] = array(
+			'id' => 2,
 			'row' => 'konto_bar',
-			'property' => 'bar',
+			'property' => '\'bar\'',
 			'typ' => 'enum',
+			'compare' => "=",
 			'label' => 'nur Einträge des Kassenbuches'
 	);
 	$filter[] = array(
+			'id' => 3,
 			'row' => 'betreff',
-			'property' => 'schueler',
+			'property' => '\'schueler\'',
 			'typ' => 'enum',
+			'compare' => "=",
 			'label' => 'nur Einträge der Kategorie Schüler'
 	);
 	$filter[] = array(
+			'id' => 4,
 			'row' => 'betreff',
-			'property' => 'lehrer',
+			'property' => '\'lehrer\'',
 			'typ' => 'enum',
+			'compare' => "=",
 			'label' => 'nur Einträge der Kategorie Lehrer'
 	);
 	$filter[] = array(
+			'id' => 5,
 			'row' => 'betreff',
-			'property' => 'sonstiges',
+			'property' => '\'sonstiges\'',
 			'typ' => 'enum',
+			'compare' => "=",
 			'label' => 'nur Einträge der Kategorie sonstiges'
 	);
+	$filter[] = array(
+			'id' => 6,
+			'row' => 'datum',
+			'property' => ' \'2017-01-01\' AND \'2018-01-01\'',
+			'typ' => 'dates',
+			'compare' => 'BETWEEN',
+			'label' => 'nur Einträge aus dem Jahr 2017'
+	);
+	$filter[] = array(
+			'id' => 7,
+			'row' => 'datum',
+			'property' => ' \'2017-08-01\' AND \'2018-07-01\'',
+			'typ' => 'dates',
+			'compare' => 'BETWEEN',
+			'label' => 'nur Einträge aus dem Schuljahr 2017/2018'
+	);
+	$filter[] = array(
+			'id' => 8,
+			'row' => 'datum',
+			'property' => ' \'2018-01-01\' AND \'2019-01-01\'',
+			'typ' => 'dates',
+			'compare' => 'BETWEEN',
+			'label' => 'nur Einträge aus dem Jahr 2018'
+	);
 	$aktiv_filter = "";
+	$params = "";
+	$get_string = (isset($_GET['filter']) ? $_GET['filter']:'');
+	$get_string2 = (isset($_GET['filter']) ? $_GET['filter']:'');
 	for($i = 0; $i < count($filter); $i++) {
-		if(isset($_GET['filter']) && isset($_GET['row']) && isset($_GET['property']) && $_GET['row'] == $filter[$i]['row'] && $_GET['property'] == $filter[$i]['property']) {
-			$aktiv_filter .= "<a href=\"index.php?page=output_finanzen&filter=1&row=".$filter[$i]['row']."&property=". $filter[$i]['property'] ."\" class=\"links2\"><img src=\"img/png_cross_24_24.png\" style=\"width:18px;heigth:18px;\">".$filter[$i]['label']."</a><br>";
+		if(strlen($get_string) > 0  && strpos($get_string, strval($filter[$i]['id'])) !== false ) {
+			$get_string = $get_string2;
+			$get_string = str_replace($filter[$i]['id'], '', $get_string);
+			$aktiv_filter .= "<a href=\"index.php?page=output_finanzen&filter=$get_string\" class=\"links2\"><img src=\"img/png_cross_24_24.png\" style=\"width:18px;heigth:18px;\">".$filter[$i]['label']."</a><br>";
+			$params .= (strlen($params) > 0 ?" AND ": '').$filter[$i]['row']." ".$filter[$i]['compare'] . " ". $filter[$i]['property'];
 		}else{
-			echo "<br><a href=\"index.php?page=output_finanzen&filter=1&row=".$filter[$i]['row']."&property=". $filter[$i]['property'] ."\" class=\"links2\">".$filter[$i]['label']."</a>";
+			echo "<br><a href=\"index.php?page=output_finanzen&filter=$get_string2".$filter[$i]['id']."\" class=\"links2\">".$filter[$i]['label']."</a>";
 		}
 	}
+//	var_dump($params);
 	echo "<br><hr><br>aktive Filter:$aktiv_filter<br><a href=\"index.php?page=output_finanzen\" class=\"links2\">kein Filter</a>";
-	if(isset($_GET['filter']) && isset($_GET['row']) && isset($_GET['property']) ) {
-	$return = query_db("SELECT finanzuebersicht.*, users.vname, users.nname, person.vname AS pvname, person.nname AS pnname FROM `finanzuebersicht`
-			LEFT JOIN `users` ON finanzuebersicht.uid = users.id LEFT JOIN `person` ON finanzuebersicht.pid = person.id  WHERE " . $_GET['row'] . " = '" . $_GET['property'] . "' ORDER BY finanzuebersicht.datum ASC;");
+	if(isset($_GET['filter']) ) {
+		$return = query_db("SELECT finanzuebersicht.*, users.vname, users.nname, person.vname AS pvname, person.nname AS pnname FROM `finanzuebersicht`
+			LEFT JOIN `users` ON finanzuebersicht.uid = users.id LEFT JOIN `person` ON finanzuebersicht.pid = person.id  WHERE $params ORDER BY finanzuebersicht.datum ASC;");
 	}else{
 		$return = query_db("SELECT finanzuebersicht.*, users.vname, users.nname, person.vname AS pvname, person.nname AS pnname FROM `finanzuebersicht`
 			LEFT JOIN `users` ON finanzuebersicht.uid = users.id LEFT JOIN `person` ON finanzuebersicht.pid = person.id ORDER BY finanzuebersicht.datum ASC;");
 	}
+	echo "<br><br><b>Hinweis:</b> Die Kontostände und Kassenstände werden auf der Basis von den angezeigten Datensätzen erstellt. 
+			Um den korrekten Stand der Kasse oder des Kontos zu sehen bitte alle Datensätze ohne Filter anzeigen lassen oder einen Finanzbericht erstellen.";
 	if ($return != false) {
 		$result = $return->fetch();
 		$sum_bar = 0;
@@ -56,6 +99,7 @@ if (isset($user) && $user->runscript()) {
   		$table_haeder = "<table class=\"table1\"><tr><th style=\"text-align: left;\">Name</th>
 		<th>Einnahmen</th><th>Ausgaben</th>
 		<th>Konto/Bar</th><th>Kategorie</th><th>Dokument</th><th>Bemerkung</th><th>Datum</th><th></th></tr>";
+//  		var_dump($result);
 		while ($result) {
 			if(isset($result_vor)) {
 				if(intval(date('m', strtotime($result_vor['datum']))) < intval(date('m', strtotime($result['datum']))) || intval(date('y', strtotime($result_vor['datum']))) < intval(date('y', strtotime($result['datum'])))) {
